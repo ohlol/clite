0e7d9f3c4c75ee60c1f69c03d764cb7f33453c3a
scott smith <scott@smith.me>
2024-02-01 12:03:33 -0800

    Print available commands if none given.

--- clite/src/__main__.py
+++ clite/src/__main__.py
@@ -1,6 +1,9 @@
+import argparse
+import os
 import sys
 from tap import Tap
 from clite.src.command import import_command
+from clite.src.commands.commands import available_commands
 from clite.src.errors import CliteExecError
 
 
@@ -10,13 +13,13 @@ class Parser(Tap):
 
 def extract_subcommand(args):
     if len(args) > 1:
-        return(args[1], args[2:])
+        return(args[0], args[1], args[2:])
     else:
-        return(None, None)
+        return(args[0], None, None)
 
 
 def main_or_fail():
-    subcommand_name, subcommand_args = extract_subcommand(sys.argv)
+    program_name, subcommand_name, subcommand_args = extract_subcommand(sys.argv)
     if subcommand_name:
         subcommand = import_command(subcommand_name)
         if subcommand:
@@ -27,7 +30,13 @@ def main_or_fail():
             except CliteExecError as e:
                 print(f"error: {e.__class__.__name__}({e})", file=sys.stderr)
         else:
-            Parser().parse_args()
+            print("error: unknown command\n")
+            print(available_commands())
+            sys.exit(1)
+    else:
+        short_program = os.path.split(program_name)[-1]
+
+        Parser(formatter_class=argparse.RawDescriptionHelpFormatter, usage=f"{short_program} [command]", epilog=available_commands()).print_help()
 
 
 if __name__ == "__main__":
